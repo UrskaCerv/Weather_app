5dfd0871480c4dc806a84cd1a63f8198adbb4bd4
UrskaCerv <72766358+UrskaCerv@users.noreply.github.com>
2020-12-05 01:12:39 +0100

    Added forecast data

--- src/index.js
+++ src/index.js
@@ -49,11 +49,67 @@ function showWeather(response) {
     .querySelector("#icon-big")
     .setAttribute("alt", response.data.weather[0].description);
 }
+function getDayOfTheWeek(timestamp) {
+  var forecastDay = new Date();
+  forecastDay.setTime(timestamp * 1000); // javascript timestamps are in milliseconds
+  forecastDay.toUTCString();
+  let dayIndex = forecastDay.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastDayName = days[dayIndex];
+  return forecastDayName;
+}
+
+function showForecast(response) {
+  event.preventDefault();
+  let forecastElements = document.querySelector("#forecast");
+  forecastElements.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 5; index++) {
+    forecast = response.data.list[index];
+
+    if (index === 0) {
+      forecastElements.innerHTML = `
+  <div class="col-1" id="blank">
+    
+  </div>`;
+    }
+
+    forecastElements.innerHTML += `
+  <div class="col-2" id="next-days">
+    <p>
+      ${getDayOfTheWeek(response.data.list[0 + 8 * index].dt)}
+      <br />
+      <img id="forecast-image" 
+      src="http://openweathermap.org/img/wn/${
+        response.data.list[4 + 8 * index].weather[0].icon
+      }@2x.png"
+      />
+      <br />
+      <strong><span class="day" id="forecastTemperature">${Math.round(
+        response.data.list[4 + 8 * index].main.temp
+      )}</span>°</strong> <span class="night" id="forecastTemperature">${Math.round(
+      response.data.list[2 + 8 * index].main.temp
+    )}</span>°
+    </p>
+  </div>
+  `;
+
+    if (index === 5) {
+      forecastElements.innerHTML = `
+  <div class="col-1" id="blank">
+  </div>;`;
+    }
+  }
+}
 
 function searchCity(city) {
-  let apiKey = "ec0871cacae579eca821990b4ac7095a";
+  let apiKey = "cddfb1e3d89e2258740a8f1797f07940";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function changeLocation(event) {
@@ -65,7 +121,7 @@ function changeLocation(event) {
 function searchPosition(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
-  let apiKey = "ec0871cacae579eca821990b4ac7095a";
+  let apiKey = "cddfb1e3d89e2258740a8f1797f07940";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showWeather);
 }
@@ -80,14 +136,16 @@ function changeUnitF(event) {
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   let temperature = document.querySelector("#temperature");
 
-  //let choose = document.querySelector("#choose");
-  //let functionCity = document.querySelector("#city");
-  //let apiKey = "cddfb1e3d89e2258740a8f1797f07940";
-  //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${functionCity}&appid=${apiKey}&units=imperial`;
-  //axios.get(apiUrl).then(showWeather);
   temperature.innerHTML = `${Math.round(fahrenheitTemperature)}°`;
   fahrenheit.innerHTML = "<strong><u>f</strong></u>";
   celsius.innerHTML = "c";
+
+  let forecastTemperature = document.querySelectorAll("#forecastTemperature");
+  console.log(forecastTemperature);
+  forecastTemperature.forEach(function (item) {
+    let currentTemp = item.innerHTML;
+    item.innerHTML = Math.round((currentTemp * 9) / 5 + 32);
+  });
 }
 
 function changeUnitC(event) {
@@ -96,16 +154,14 @@ function changeUnitC(event) {
   temperature.innerHTML = `${Math.round(celsiusTemperature)}°`;
   fahrenheit.innerHTML = "f";
   celsius.innerHTML = "<strong><u>c</strong></u>";
-  //changeUnitCAutomatic();
-}
 
-//function changeUnitCAutomatic() {
-//let temperature = document.querySelector("#temperature");
-//let celsius = document.querySelector("#celsius");
-//let fahrenheit = document.querySelector("#fahrenheit");
-//let functionCity = document.querySelector("#city");
-// let apiKey = "cddfb1e3d89e2258740a8f1797f07940";
-// let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${functionCity}&appid=${apiKey}&units=metric`;
+  let forecastTemperature = document.querySelectorAll("#forecastTemperature");
+  console.log(forecastTemperature);
+  forecastTemperature.forEach(function (item) {
+    let currentTemp = item.innerHTML;
+    item.innerHTML = Math.round(((currentTemp - 32) * 5) / 9);
+  });
+}
 
 // axios.get(apiUrl).then(showWeather);
 temperature.innerHTML = 18 + "°";
@@ -132,9 +188,9 @@ geolocation.addEventListener("click", geolocate);
 let celsiusTemperature = null;
 let farenheit = document.querySelector("#farenheit");
 fahrenheit.addEventListener("click", changeUnitF);
-
-//fahrenheit
 let celsuis = document.querySelector("#celsuis");
 celsius.addEventListener("click", changeUnitC);
+
+//celsius vs fahrenheit forecast
 
 searchCity("Geneve");
